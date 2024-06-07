b37e360879be2ec468dabef4c9fbb85a0c53fc41
Carlo Bruno <carlofbruno@gmail.com>
2024-06-07 08:41:49 -0500

    Some boilerplate cleanup

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex items-center p-4">
+    <div className="flex items-center p-4 justify-evenly">
       <Image src="/tools_icon.svg" width={32} height={32} alt={""} />
-      <h1 className="text-xl font-bold pl-4 ita">Helix Toolkit</h1>
+      <h1 className="text-xl font-bold">Helix Toolkit</h1>
     </div>
   );
 }
--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -2,9 +2,9 @@ import Logo from "./Logo";
 
 export default function SideBar() {
   return (
-    <div className="w-72 bg-slate-500 min-h-screen shrink-0">
+    <div className="w-64 bg-slate-500 min-h-screen shrink-0 px-4">
       <Logo />
-      <p> Controller View</p>
+      <p> Footswitch View</p>
       <p> Preset Diff</p>
       <p>Block Pastebin</p>
       <p>Tuner</p>
